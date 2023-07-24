3a94f9b23823c016fc0c06959daeec904b338d1c
Roman Ortega Mondaca <romtega88@gmail.com>
2023-07-24 14:43:21 -0600

    Basic operations and user access

---
+++ js/db.js
@@ -0,0 +1,27 @@
+const users = [
+  {
+    userId: "Usuario 1",
+    password: "Contraseña1",
+    savings: 100,
+  },
+  {
+    userId: "Usuario 2",
+    password: "Contraseña2",
+    savings: 200,
+  },
+  {
+    userId: "Usuario 3",
+    password: "Contraseña3",
+    savings: 300,
+  },
+  {
+    userId: "Usuario 4",
+    password: "Contraseña4",
+    savings: 400,
+  },
+  {
+    userId: "Usuario 5",
+    password: "Contraseña5",
+    savings: 500,
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
-const open = document.querySelector("#btn-confirm");
-const close = document.querySelector("#btn-clear");
-const account = document.querySelector("#account-section");
+"use strict";
 
-open.addEventListener("click", function () {
-  account.classList.remove("hidden");
+const login = document.querySelector("#btn-login");
+const logout = document.querySelector("#btn-logout");
+const deposit = document.querySelector("#btn-deposit");
+const withdraw = document.querySelector("#btn-withdraw");
+const confirm = document.querySelector("#btn-confirm");
+const clear = document.querySelector("#btn-clear");
+const accountCard = document.querySelector("#account-section");
+const userSign = document.querySelector("#user-id");
+const userPasswordSign = document.querySelector("#user-password");
+const username = document.querySelector("#username");
+const userSavings = document.querySelector("#user-savings");
+const amount = document.querySelector("#user-amount");
+const newSavings = document.querySelector("#user-new-savings");
+
+logout.addEventListener("click", function () {
+  accountCard.classList.add("hidden");
 });
 
-close.addEventListener("click", function () {
-  account.classList.add("hidden");
+login.addEventListener("click", function () {
+  for (let i = 0; i < users.length; i++) {
+    if (
+      userSign.value == users[i].userId &&
+      userPasswordSign.value == users[i].password
+    ) {
+      accountCard.classList.remove("hidden");
+      username.textContent = users[i].userId;
+      userSavings.textContent = users[i].savings;
+
+      deposit.addEventListener("click", function () {
+        newSavings.textContent = users[i].savings + Number(amount.value);
+      });
+
+      withdraw.addEventListener("click", function () {
+        newSavings.textContent = users[i].savings - Number(amount.value);
+      });
+
+      clear.addEventListener("click", function () {
+        amount.value = "";
+        newSavings.textContent = "";
+      });
+    }
+  }
 });
